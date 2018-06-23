9de8cc5aea59e491023c5416bf8b4de441a7514b
Elliot Yandzio <elliotyandzio@gmail.com>
2018-06-23 18:38:35 +0100

    changed typo in env file

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 4000;
-const env = process.env.NODE_END || 'dev';
-const dbURI = process.env.MONGODB_URI|| `mongodb://localhost/scoutingApp-${env}`;
+const env = process.env.NODE_ENV || 'dev';
 const secret = process.env.SECRET || 'scoutingApp secret!';
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/scoutingApp-${env}`;
 
 module.exports = {port, dbURI, secret};
